0bb9a95c9a54945c28fc6c082009c788f1b365bb
Mike Haggerty <ghoti143@gmail.com>
2018-04-17 18:01:37 -0400

    Update sirspamalot.sh

--- sirspamalot.sh
+++ sirspamalot.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 START=$(date +%s)
-taskset -c 0 ./spammy.sh 5000 foo > out1.txt &
-taskset -c 1 ./spammy.sh 5000 bar > out2.txt &
-taskset -c 2 ./spammy.sh 5000 cho > out3.txt &
-taskset -c 3 ./spammy.sh 5000 woo > out4.txt &
+./spammy.sh 2000 spammy1foo &
+./spammy.sh 2000 spammy1bar &
+./spammy.sh 2000 spammy1cho &
+./spammy.sh 2000 spammy1wuo &
+./spammy.sh 2000 spammy1wbo &
+./spammy.sh 2000 spammy1wco &
 wait
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "It took $DIFF seconds to spam 20000 transactions"  
+echo "It took $DIFF seconds to spam 12000 transactions"    
